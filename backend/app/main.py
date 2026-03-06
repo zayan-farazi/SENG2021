ccb82bbe38915dc23ba4d1af66d90f2d12a0f0c0
Natalia Michel Lemuz <z5430065@unsw.edu.au>
2026-03-06 04:18:52 -0400

    bug fix: Forgot to add supabase to backend requirements

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from other import saveOrderDetails
 
 app = FastAPI()
 
--- backend/app/other.py
+++ backend/app/other.py
@@ -1,19 +1,47 @@
 from supabase import create_client, Client
+import datetime
 
 SUPABASE_URL='https://zfkanfxuznozqpqfxbly.supabase.co'
 SUPABASE_KEY='sb_publishable_jhMhN4VwzVrroJ202_ahAA_pChVwwnZ'
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-new = {'buyername': 'Jane Doe', 
-       'sellername': 'John Doe',
-       'deliverystreet': '221B Baker St',
-       'deliverycity': 'Sydney',
-       'deliverypostcode': '1234',
-       'deliverycountry': 'Australia',
-       'status': 'pending',
-       'notes': 'boooop'}
+# creates dictionary with table names and query information saved.
+def defineQuery(buyerName, sellerName, deliveryStreet, 
+                     deliveryCity, deliveryPostcode, deliveryCountry, notes,
+                     timeIssued, status):
+    return {'buyername': buyerName, 
+       'sellername': sellerName,
+       'deliverystreet': deliveryStreet,
+       'deliverycity': deliveryCity,
+       'deliverypostcode': deliveryPostcode,
+       'deliverycountry': deliveryCountry,
+       'timeIssued': timeIssued,
+       'lastedited': timeIssued,
+       'status': timeIssued,
+       'notes': notes
+    }
 
-supabase.table('orders').insert(new).execute()
-results = supabase.table('orders').select('*').execute()
-print(results)
+# saves order information as new entry 
+def saveOrder(buyerName, sellerName, deliveryStreet, 
+                     deliveryCity, deliveryPostcode, deliveryCountry, notes,
+                     timeIssued=datetime.now(), status='Pending'):
+    
+    query = defineQuery(buyerName, sellerName, deliveryStreet, 
+                     deliveryCity, deliveryPostcode, deliveryCountry, notes,
+                     timeIssued, status)
+    supabase.table('orders').insert(query).execute()
+
+# Input only the necessary fields
+# Returns all orders with given filters, NULL if no order matches the filters
+def retrieveOrder(buyerName='*', sellerName='*', deliveryStreet='*', 
+                     deliveryCity='*', deliveryPostcode='*', deliveryCountry='*', notes='*',
+                     timeIssued='*', status='*'):
+    query= query = defineQuery(buyerName, sellerName, deliveryStreet, 
+                     deliveryCity, deliveryPostcode, deliveryCountry, notes,
+                     timeIssued, status)
+    supabase.table('orders').select('*').eq(query):
+
+# saves order detail information
+# MUST BE ENTERED ONE LINE AT A TIME
+def saveOrderDetails()
