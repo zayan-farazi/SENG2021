c497ef7c978ce2e93c9419ce18df8fa3f739067c
Natalia Michel Lemuz <z5430065@unsw.edu.au>
2026-03-08 19:28:06 -0400

    added DBInfo function to help with debugging, and findOrderDetails. findOrders now also returns order line details when only one order is found. saveOrder and saveOrderDetails can now be used to update and add info to db

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,30 @@
 from fastapi import FastAPI
-#from other import saveOrder, saveOrderDetails, findOrders
+from other import findOrders  # , saveOrder, saveOrderDetails, DBInfo
+# import datetime
 
 app = FastAPI()
 
 
 @app.get("/")
 def root():
+    # how to save order example
+    # this works but pls make sure all details are correct before running the functions
+    # and do a try except so we don't up w rubbish data / duplicate info
 
-    return {"message": "hello world"}
+    # default currency is AUD
+
+    """
+    orderid = saveOrder(
+        "Rita",
+        "Tina",
+        "321 Road",
+        "Sydney",
+        344,
+        "Australia",
+        datetime.datetime.now().isoformat(),
+        "Pending",
+        "pls work",
+    )
+    saveOrderDetails(orderid, "pears", "def", 5.9, 9.8)
+    """
+    return findOrders(orderId="12")
--- backend/app/other.py
+++ backend/app/other.py
@@ -1,84 +1,135 @@
 from supabase import create_client, Client
 import datetime
 
-SUPABASE_URL='https://zfkanfxuznozqpqfxbly.supabase.co'
-SUPABASE_KEY='sb_publishable_jhMhN4VwzVrroJ202_ahAA_pChVwwnZ'
+SUPABASE_URL = "https://zfkanfxuznozqpqfxbly.supabase.co"
+SUPABASE_KEY = "sb_publishable_jhMhN4VwzVrroJ202_ahAA_pChVwwnZ"
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-# saves order information and returns order Id
-def saveOrder(buyername, sellername, deliverystreet, deliverycity,
-              deliverypostcode, deliverycountry, notes,
-              issueDate=None, status='Pending'):
-    
+
+# saves or updates order information and returns order Id
+def saveOrder(
+    buyername,
+    sellername,
+    deliverystreet,
+    deliverycity,
+    deliverypostcode,
+    deliverycountry,
+    issueDate,
+    status,
+    notes,
+    currency="AUD",
+):
+
     if issueDate is None:
         issueDate = datetime.datetime.now()
 
     query = {
-        'buyername': buyername,
-        'sellername': sellername,
-        'deliverystreet': deliverystreet,
-        'deliverycity': deliverycity,
-        'deliverypostcode': deliverypostcode,
-        'deliverycountry': deliverycountry,
-        'status': status,
-        'notes': notes,
-        'issuedate': issueDate.isoformat(),
-        'lastchanged': datetime.datetime.now().isoformat()
+        "buyername": buyername,
+        "sellername": sellername,
+        "deliverystreet": deliverystreet,
+        "deliverycity": deliverycity,
+        "deliverypostcode": deliverypostcode,
+        "deliverycountry": deliverycountry,
+        "status": status,
+        "notes": notes,
+        "currency": currency,
+        "issuedate": issueDate.isoformat(),
+        "lastchanged": datetime.datetime.now().isoformat(),
     }
 
     try:
-        response = supabase.table('orders').insert(query).execute()
-        return response.data[0]['id']
+        response = supabase.table("orders").upsert(query).execute()
+        return response.data[0]["id"]
     except Exception as e:
         raise RuntimeError(f"Failed to save order: {e}")
 
-# saves a single line of order details, does not return anything
+
+# saves or updates a single line of order details, does not return anything
+# NEEDS ORDER ID TO ATTATCH TO
 def saveOrderDetails(orderId, productName, unitCode, quantity, unitPrice):
     if orderId is None:
-        raise ValueError("Failed to parse order details: orderId can\'t be empty")
-    
+        raise ValueError("Failed to parse order details: orderId can't be empty")
+
     query = {
-        'orderid': orderId,
-        'productname': productName,
-        'unitcode': unitCode,
-        'quantity': quantity,
-        'unitprice': unitPrice
+        "orderid": orderId,
+        "productname": productName,
+        "unitcode": unitCode,
+        "quantity": float(quantity),
+        "unitprice": float(unitPrice),
     }
 
     try:
-       supabase.table('orderdetails').insert(query).execute()
+        supabase.table("orderdetails").upsert(query).execute()
     except Exception as e:
-       raise RuntimeError(f"Failed to save order details: {e}")
+        raise RuntimeError(f"Failed to save order details: {e}")
 
-# (greedily) returns all tuples mathing the filter(s). 
+
+# (greedily) returns all tuples mathing the filter(s).
 # must write the variable name as all fields have empty default values
-def findOrders(buyername=None, sellername=None, deliverystreet=None, deliverycity=None,
-              deliverypostcode=None, deliverycountry=None, notes=None,
-              issueDate=None, lastChanged=None, status=None):
-    query = supabase.table('orders').select('*')
+# if there are several matches, it returns list (up to 1000 tuples)
+# if there is only one match, it returns it as well as its order line list
+def findOrders(
+    orderId=None,
+    buyername=None,
+    sellername=None,
+    deliverystreet=None,
+    deliverycity=None,
+    deliverypostcode=None,
+    deliverycountry=None,
+    notes=None,
+    issueDate=None,
+    lastChanged=None,
+    status=None,
+):
+    query = supabase.table("orders").select("*", count="exact")
 
-    if buyername:    
-        query = query.eq('buyername', buyername)
-    if sellername:   
-        query = query.eq('sellername', sellername)
-    if deliverystreet:      
-        query = query.eq('deliverystreet', deliverystreet)
-    if deliverycity: 
-        query = query.eq('deliverycity', deliverycity)
-    if deliverypostcode:    
-        query = query.eq('deliverypostcode', deliverypostcode)
-    if deliverycountry:     
-        query = query.eq('deliverycountry', deliverycountry)
-    if notes: 
-        query = query.ilike('notes', notes)
-    if status:       
-        query = query.eq('status', status)
-    if issueDate:    
-        query = query.eq('issuedate', issueDate)
-    if lastChanged:  
-        query = query.eq('lastchanged', lastChanged)
-    if status:       
-        query = query.eq('status', status)
+    if orderId:
+        query = query.eq("id", orderId)
+    if buyername:
+        query = query.eq("buyername", buyername)
+    if sellername:
+        query = query.eq("sellername", sellername)
+    if deliverystreet:
+        query = query.eq("deliverystreet", deliverystreet)
+    if deliverycity:
+        query = query.eq("deliverycity", deliverycity)
+    if deliverypostcode:
+        query = query.eq("deliverypostcode", deliverypostcode)
+    if deliverycountry:
+        query = query.eq("deliverycountry", deliverycountry)
+    if notes:
+        query = query.ilike("notes", notes)
+    if status:
+        query = query.eq("status", status)
+    if issueDate:
+        query = query.eq("issuedate", issueDate)
+    if lastChanged:
+        query = query.eq("lastchanged", lastChanged)
+    if status:
+        query = query.eq("status", status)
 
-    return query.execute()
+    res = query.execute()
+    if res.count == 0:
+        return "No orders found"
+
+    if res.count > 1:
+        return res
+    return res, findOrderDetails(res.data[0]["id"])
+
+
+# looks for order detail list through order id
+def findOrderDetails(orderId):
+    return (
+        supabase.table("orderdetails")
+        .select("productname", "unitcode", "quantity", "unitprice")
+        .eq("orderid", orderId)
+        .execute()
+    )
+
+
+# mostly for debug purposes, returns all information stored in both databases
+def DBInfo():
+    orders = supabase.table("orders").select("*").execute()
+    orderDetails = supabase.table("orderdetails").select("*").execute()
+    return orders, orderDetails
