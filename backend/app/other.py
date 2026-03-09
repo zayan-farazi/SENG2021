26e49d6a1ec6ecc8ce142d32867f007af8595a8b
Natalia Michel Lemuz <z5430065@unsw.edu.au>
2026-03-08 20:32:56 -0400

    linting

--- backend/app/other.py
+++ backend/app/other.py
@@ -1,20 +1,21 @@
-from supabase import create_client, Client
-import datetime
+from supabase import Client, create_client
 
-SUPABASE_URL='https://zfkanfxuznozqpqfxbly.supabase.co'
-SUPABASE_KEY='sb_publishable_jhMhN4VwzVrroJ202_ahAA_pChVwwnZ'
+SUPABASE_URL = "https://zfkanfxuznozqpqfxbly.supabase.co"
+SUPABASE_KEY = "sb_publishable_jhMhN4VwzVrroJ202_ahAA_pChVwwnZ"
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-new = {'buyername': 'Jane Doe', 
-       'sellername': 'John Doe',
-       'deliverystreet': '221B Baker St',
-       'deliverycity': 'Sydney',
-       'deliverypostcode': '1234',
-       'deliverycountry': 'Australia',
-       'status': 'pending',
-       'notes': 'boooop'}
+new = {
+    "buyername": "Jane Doe",
+    "sellername": "John Doe",
+    "deliverystreet": "221B Baker St",
+    "deliverycity": "Sydney",
+    "deliverypostcode": "1234",
+    "deliverycountry": "Australia",
+    "status": "pending",
+    "notes": "boooop",
+}
 
-supabase.table('orders').insert(new).execute()
-results = supabase.table('orders').select('*').execute()
+supabase.table("orders").insert(new).execute()
+results = supabase.table("orders").select("*").execute()
 print(results)
