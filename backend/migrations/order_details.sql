c817e25367aef7cdc5b3a9cba95c07db57b786bd
Natalia Michel Lemuz <z5430065@unsw.edu.au>
2026-03-08 20:51:50 -0400

    schema fixes

--- backend/migrations/order_details.sql
+++ backend/migrations/order_details.sql
@@ -2,7 +2,7 @@ CREATE TABLE orderDetails (
   orderId INT,
   productName TEXT NOT NULL,
   unitCode TEXT,
-  quantity INT NOT NULL,
-  unitPrice INT NOT NULL,
+  quantity FLOAT NOT NULL,
+  unitPrice FLOAT NOT NULL,
   FOREIGN KEY (orderId) REFERENCES orders(id)
 );
