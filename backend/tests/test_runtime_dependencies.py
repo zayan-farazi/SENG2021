467ef5c05c1125adf1563ff227e7af9d78023871
Zayan Farazi <zayan.farazi1@gmail.com>
2026-03-14 16:50:46 +1100

    Add live integration test flows

--- backend/tests/test_runtime_dependencies.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-import importlib.util
-
-
-def test_websocket_transport_dependency_is_installed():
-    assert importlib.util.find_spec("websockets") is not None
